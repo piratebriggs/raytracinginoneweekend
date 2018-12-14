6e4b4d3d7747e2826efd43f3e8852c6712dab0d0
Antony Briggs <antony.briggs@gmail.com>
2018-12-14 18:43:00 +0000

    Anti alias (random method didn't work!)

---
+++ Camera.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+
+namespace raytracinginoneweekend
+{
+    public class Camera
+    {
+        public Camera()
+        {
+            Origin = new Vector3(0.0f);
+            LowerLeftCorner = new Vector3(-2.0f, -1.0f, -1.0f);
+            Horizontal = new Vector3(4, 0, 0);
+            Vertical = new Vector3(0, 2, 0);
+        }
+
+        public Ray GetRay(float u, float v)
+        {
+            return new Ray(Origin, LowerLeftCorner + u * Horizontal + v * Vertical - Origin);
+        }
+
+        Vector3 Origin { get; set; }
+        Vector3 LowerLeftCorner { get; set; }
+        Vector3 Horizontal { get; set; }
+        Vector3 Vertical { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@ namespace raytracinginoneweekend
 
         static Vector3 Color(Ray r, IList<IHitable> world) {
             var rec = new HitRecord();
-            if(world.Hit(r,0.0f, float.MaxValue,ref rec))
+            if (world.Hit(r, 0.0f, float.MaxValue, ref rec))
             {
                 return 0.5f * new Vector3(rec.Normal.X + 1, rec.Normal.Y + 1, rec.Normal.Z + 1);
             }
@@ -37,17 +37,23 @@ namespace raytracinginoneweekend
         static void Main(string[] args)
         {
             var x = new Vector3(0, 0, 0);
-            int nx = 2000;
-            int ny = 1000;
-
-            var origin = new Vector3(0.0f);
-            var lower_left_corner = new Vector3(-2.0f, -1.0f, -1.0f);
-            var horz = new Vector3(4,0,0);
-            var vert = new Vector3(0,2,0);
+            int nx = 200;
+            int ny = 100;
 
             var world = new List<IHitable>();
             world.Add(new Sphere(new Vector3(0, 0, -1), 0.5f));
             world.Add(new Sphere(new Vector3(0, -100.5f, -1), 100));
+
+            var cam = new Camera();
+            var rnd = new Random(123);
+
+            var antiAlias = new List<Tuple<float, float>>() {
+                { new Tuple<float, float>(0.25f, 0.25f) },
+                { new Tuple<float, float>(0.25f, 0.75f) },
+                { new Tuple<float, float>(0.75f, 0.25f) },
+                { new Tuple<float, float>(0.75f, 0.75f) },
+                { new Tuple<float, float>(0.5f, 0.5f) }
+            };
 
             using (Image<Rgba32> image = new Image<Rgba32>(nx, ny))
             {
@@ -55,11 +61,16 @@ namespace raytracinginoneweekend
                     var j = ny - index;
                     for (int i = 0; i < nx; i++)
                     {
-                        float u = (float)i / (float)nx;
-                        float v = (float)j / (float)ny;
+                        var col = new Vector3(0);
 
-                        var r = new Ray(origin, lower_left_corner + u * horz + v * vert);
-                        var col = Color(r, world);
+                        foreach(var point in antiAlias)
+                        {
+                            float u = ((float)i + point.Item1) / (float)nx;
+                            float v = ((float)j + point.Item2) / (float)ny;
+                            var r = cam.GetRay(u, v);
+                            col += Color(r, world);
+                        }
+                        col /= (float)antiAlias.Count;
 
                         image[i, ny-j] = new Rgba32(col);
                     }
