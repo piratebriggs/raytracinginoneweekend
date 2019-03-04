c78cb5166f06b271f41fb7acdff9c612fd1ba518
Antony Briggs <antony.briggs@gmail.com>
2019-03-04 17:28:26 +0000

    ssBVH implementation from https://www.codeproject.com/Articles/832957/Dynamic-Bounding-Volume-Hiearchy-in-Csharp

---
+++ ssBVH/SSAABB.cs
@@ -0,0 +1,155 @@
+﻿// Copyright(C) David W. Jeske, 2013
+// Released to the public domain. 
+
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace SimpleScene
+{
+    public struct SSAABB : IEquatable<SSAABB> {
+        public Vector3 Min;
+        public Vector3 Max;
+
+        public SSAABB(float min = float.PositiveInfinity, float max = float.NegativeInfinity) {
+            Min = new Vector3(min);
+            Max = new Vector3(max);
+        }
+
+		public SSAABB(Vector3 min, Vector3 max)
+		{
+			Min = min;
+			Max = max;
+		}
+
+        public void Combine(ref SSAABB other) {
+            Min = Vector3.Min(Min, other.Min);
+            Max = Vector3.Max(Max, other.Max);
+        }
+
+        public bool IntersectsSphere(Vector3 origin, float radius) {
+            if ( 
+                (origin.X + radius < Min.X) ||
+                (origin.Y + radius < Min.Y) ||
+                (origin.Z + radius < Min.Z) ||
+                (origin.X - radius > Max.X) ||
+                (origin.Y - radius > Max.Y) ||
+                (origin.Z - radius > Max.Z)
+               ) {
+               return false;
+            } else {
+               return true;
+            }
+        }
+
+		public bool IntersectsSphere(SSSphere sphere)
+		{
+			return IntersectsSphere (sphere.center, sphere.radius);
+		}
+
+        public bool IntersectsAABB(SSAABB box) {
+            return ( (Max.X > box.Min.X) && (Min.X < box.Max.X) &&
+                     (Max.Y > box.Min.Y) && (Min.Y < box.Max.Y) &&
+                     (Max.Z > box.Min.Z) && (Min.Z < box.Max.Z));
+        }
+
+        public bool Equals(SSAABB other) {
+            return 
+                (Min.X == other.Min.X) &&
+                (Min.Y == other.Min.Y) &&
+                (Min.Z == other.Min.Z) &&
+                (Max.X == other.Max.X) &&
+                (Max.Y == other.Max.Y) &&
+                (Max.Z == other.Max.Z);
+        }
+
+        public void UpdateMin(Vector3 localMin) {
+            Min = Vector3.Min(Min, localMin);
+        }
+
+        public void UpdateMax(Vector3 localMax) {
+            Max = Vector3.Max(Max, localMax);
+        }
+
+        public Vector3 Center() {
+            return (Min + Max) / 2f;
+        }
+
+        public Vector3 Diff() {
+            return Max - Min;
+        }
+
+		public SSSphere ToSphere()
+		{
+			float r = (Diff ().Length() + 0.001f)/2f;
+			return new SSSphere (Center (), r);
+		}
+
+        internal void ExpandToFit(SSAABB b) {
+            if (b.Min.X < this.Min.X) { this.Min.X = b.Min.X; }            
+            if (b.Min.Y < this.Min.Y) { this.Min.Y = b.Min.Y; }            
+            if (b.Min.Z < this.Min.Z) { this.Min.Z = b.Min.Z; }
+            
+            if (b.Max.X > this.Max.X) { this.Max.X = b.Max.X; }
+            if (b.Max.Y > this.Max.Y) { this.Max.Y = b.Max.Y; }
+            if (b.Max.Z > this.Max.Z) { this.Max.Z = b.Max.Z; }                        
+        }
+
+        public SSAABB ExpandedBy(SSAABB b) {
+            SSAABB newbox = this;
+            if (b.Min.X < newbox.Min.X) { newbox.Min.X = b.Min.X; }            
+            if (b.Min.Y < newbox.Min.Y) { newbox.Min.Y = b.Min.Y; }            
+            if (b.Min.Z < newbox.Min.Z) { newbox.Min.Z = b.Min.Z; }
+            
+            if (b.Max.X > newbox.Max.X) { newbox.Max.X = b.Max.X; }
+            if (b.Max.Y > newbox.Max.Y) { newbox.Max.Y = b.Max.Y; }
+            if (b.Max.Z > newbox.Max.Z) { newbox.Max.Z = b.Max.Z; }   
+
+            return newbox;
+        }
+
+		public void ExpandBy(SSAABB b) 
+		{
+			this = this.ExpandedBy (b);
+		}
+
+		public static SSAABB FromSphere(Vector3 pos, float radius) {
+            SSAABB box;
+            box.Min.X = pos.X - radius;
+            box.Max.X = pos.X + radius;
+            box.Min.Y = pos.Y - radius;
+            box.Max.Y = pos.Y + radius;
+            box.Min.Z = pos.Z - radius;
+            box.Max.Z = pos.Z + radius;
+
+            return box;
+        }
+
+        private static readonly Vector4[] c_homogenousCorners = {
+            new Vector4(-1f, -1f, -1f, 1f),
+            new Vector4(-1f, 1f, -1f, 1f),
+            new Vector4(1f, 1f, -1f, 1f),
+            new Vector4(1f, -1f, -1f, 1f),
+
+            new Vector4(-1f, -1f, 1f, 1f),
+            new Vector4(-1f, 1f, 1f, 1f),
+            new Vector4(1f, 1f, 1f, 1f),
+            new Vector4(1f, -1f, 1f, 1f),
+        };
+
+        /*
+        public static SSAABB FromFrustum(ref Matrix4 axisTransform, ref Matrix4 modelViewProj) {
+            SSAABB ret = new SSAABB(float.PositiveInfinity, float.NegativeInfinity);
+            Matrix4 inverse = modelViewProj;
+            inverse.Invert();
+            for (int i = 0; i < c_homogenousCorners.Length; ++i) {
+                Vector4 corner = Vector4.Transform(c_homogenousCorners [i], inverse);
+                Vector3 transfPt = Vector3.Transform(corner.Xyz / corner.W, axisTransform);
+                ret.UpdateMin(transfPt);
+                ret.UpdateMax(transfPt);
+            }
+            return ret;
+        }
+        */
+    }
+}
---
+++ ssBVH/ssBVH.cs
@@ -0,0 +1,182 @@
+﻿// Copyright(C) David W. Jeske, 2014, and released to the public domain. 
+//
+// Dynamic BVH (Bounding Volume Hierarchy) using incremental refit and tree-rotations
+//
+// initial BVH build based on: Bounding Volume Hierarchies (BVH) – A brief tutorial on what they are and how to implement them
+//              http://www.3dmuve.com/3dmblog/?p=182
+//
+// Dynamic Updates based on: "Fast, Effective BVH Updates for Animated Scenes" (Kopta, Ize, Spjut, Brunvand, David, Kensler)
+//              https://github.com/jeske/SimpleScene/blob/master/SimpleScene/Util/ssBVH/docs/BVH_fast_effective_updates_for_animated_scenes.pdf
+//
+// see also:  Space Partitioning: Octree vs. BVH
+//            http://thomasdiewald.com/blog/?p=1488
+//
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+
+// TODO: handle merge/split when LEAF_OBJ_MAX > 1 and objects move
+// TODO: add sphere traversal
+
+namespace SimpleScene.Util.ssBVH
+{
+    public enum Axis {
+        X,Y,Z,
+    }
+
+    public interface SSBVHNodeAdaptor<GO> {
+        ssBVH<GO> BVH { get; }
+        void setBVH(ssBVH<GO> bvh);
+        Vector3 objectpos(GO obj);
+        float radius(GO obj);
+        void mapObjectToBVHLeaf(GO obj, ssBVHNode<GO> leaf);
+        void unmapObject(GO obj);
+        void checkMap(GO obj);
+        ssBVHNode<GO> getLeaf(GO obj);
+    }
+
+    public class ssBVH<GO>
+    {
+        public ssBVHNode<GO> rootBVH;
+        public SSBVHNodeAdaptor<GO> nAda;
+        public readonly int LEAF_OBJ_MAX;
+        public int nodeCount = 0;
+        public int maxDepth = 0;
+
+        public HashSet<ssBVHNode<GO>> refitNodes = new HashSet<ssBVHNode<GO>>();
+
+        public delegate bool NodeTest(SSAABB box);
+
+        // internal functional traversal...
+        private void _traverse(ssBVHNode<GO> curNode, NodeTest hitTest, List<ssBVHNode<GO>> hitlist) {
+            if (curNode == null) { return; }
+            if (hitTest(curNode.box)) {
+                hitlist.Add(curNode);
+                _traverse(curNode.left,hitTest,hitlist);
+                _traverse(curNode.right,hitTest,hitlist);
+            }
+        }
+
+        // public interface to traversal..
+        public List<ssBVHNode<GO>> traverse(NodeTest hitTest) {
+            var hits = new List<ssBVHNode<GO>>();
+            this._traverse(rootBVH,hitTest,hits);
+            return hits;
+        }
+        
+        // left in for compatibility..
+        public List<ssBVHNode<GO>> traverseRay(SSRay ray) {
+            float tnear = 0f, tfar = 0f;
+
+            return traverse( box => intersectRayAABox1(ray,box,ref tnear, ref tfar) );
+        }
+
+        public List<ssBVHNode<GO>> traverse(SSRay ray) {
+            float tnear = 0f, tfar = 0f;
+
+            return traverse( box => intersectRayAABox1(ray,box,ref tnear, ref tfar) );
+        }
+        public static bool intersectRayAABox1(SSRay ray, SSAABB box, ref float tnear, ref float tfar)
+        {
+            // r.dir is unit direction vector of ray
+            Vector3 dirfrac = new Vector3();
+            float t;
+            dirfrac.X = 1.0f / ray.dir.X;
+            dirfrac.Y = 1.0f / ray.dir.Y;
+            dirfrac.Z = 1.0f / ray.dir.Z;
+            // lb is the corner of AABB with minimal coordinates - left bottom, rt is maximal corner
+            // r.org is origin of ray
+            float t1 = (box.Min.X - ray.pos.X) * dirfrac.X;
+            float t2 = (box.Max.X - ray.pos.X) * dirfrac.X;
+            float t3 = (box.Min.Y - ray.pos.Y) * dirfrac.Y;
+            float t4 = (box.Max.Y - ray.pos.Y) * dirfrac.Y;
+            float t5 = (box.Min.Z - ray.pos.Z) * dirfrac.Z;
+            float t6 = (box.Max.Z - ray.pos.Z) * dirfrac.Z;
+
+            float tmin = Math.Max(Math.Max(Math.Min(t1, t2), Math.Min(t3, t4)), Math.Min(t5, t6));
+            float tmax = Math.Min(Math.Min(Math.Max(t1, t2), Math.Max(t3, t4)), Math.Max(t5, t6));
+
+            // if tmax < 0, ray (line) is intersecting AABB, but whole AABB is behing us
+            if (tmax < 0)
+            {
+                t = tmax;
+                return false;
+            }
+
+            // if tmin > tmax, ray doesn't intersect AABB
+            if (tmin > tmax)
+            {
+                t = tmax;
+                return false;
+            }
+
+            t = tmin;
+            return true;
+
+        }
+
+        public List<ssBVHNode<GO>> traverse(SSAABB volume) {
+            return traverse( box => box.IntersectsAABB(volume) );            
+        }
+
+        /// <summary>
+        /// Call this to batch-optimize any object-changes notified through 
+        /// ssBVHNode.refit_ObjectChanged(..). For example, in a game-loop, 
+        /// call this once per frame.
+        /// </summary>
+
+        public void optimize() {  
+            if (LEAF_OBJ_MAX != 1) {
+                throw new Exception("In order to use optimize, you must set LEAF_OBJ_MAX=1");
+            }
+                  
+            while (refitNodes.Count > 0) {                
+                int maxdepth = refitNodes.Max( n => n.depth );
+            
+                var sweepNodes = refitNodes.Where( n => n.depth == maxdepth ).ToList();
+                sweepNodes.ForEach( n => refitNodes.Remove(n) );
+
+                sweepNodes.ForEach( n => n.tryRotate(this) );                
+            }            
+        }
+
+        public void addObject(GO newOb) {
+            SSAABB box = SSAABB.FromSphere(nAda.objectpos(newOb),nAda.radius(newOb));
+            float boxSAH = ssBVHNode<GO>.SA(ref box);
+            rootBVH.addObject(nAda,newOb, ref box, boxSAH);
+        }
+
+        public void removeObject(GO newObj) {
+            var leaf = nAda.getLeaf(newObj);
+            leaf.removeObject(nAda,newObj);
+        }
+
+        public int countBVHNodes() {
+            return rootBVH.countBVHNodes();
+        }
+
+        /// <summary>
+        /// initializes a BVH with a given nodeAdaptor, and object list.
+        /// </summary>
+        /// <param name="nodeAdaptor"></param>
+        /// <param name="objects"></param>
+        /// <param name="LEAF_OBJ_MAX">WARNING! currently this must be 1 to use dynamic BVH updates</param>
+        public ssBVH(SSBVHNodeAdaptor<GO> nodeAdaptor, List<GO> objects, int LEAF_OBJ_MAX = 1) {
+            this.LEAF_OBJ_MAX = LEAF_OBJ_MAX;
+            nodeAdaptor.setBVH(this);
+            this.nAda = nodeAdaptor;
+            
+            if (objects.Count > 0) {
+                rootBVH = new ssBVHNode<GO>(this,objects);            
+            } else {                
+                rootBVH = new ssBVHNode<GO>(this);
+                rootBVH.gobjects = new List<GO>(); // it's a leaf, so give it an empty object list
+            }
+        }       
+    }   
+}
